bc99bec19a94f5c92a1c31f46277e5dde780c1ee
leoanjossantos <leoanjossantos@gmail.com>
2016-04-17 22:18:36 -0300

    Insert and list through a firebase database.

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,22 +1,41 @@
-angular.module('app.controllers', [])
-  
+angular.module('app.controllers', ['ionic', 'firebase'])
+.factory('Lanches', ['$firebaseArray', function($firebaseArray) {
+  var lanchesRef = new Firebase('https://oquetemnacestadachap.firebaseio.com/lanches');
+  return $firebaseArray(lanchesRef);
+}])
+
 .controller('entrarCtrl', function($scope) {
+})
+
+.controller('tirarFotoDoLancheCtrl', function($scope, $ionicListDelegate, Lanches) {
+
+  $scope.lanches = Lanches;
+
+  $scope.addItem = function() {
+    var descricao = prompt('Descrição do lanche');
+    if(descricao){
+      $scope.lanches.$add({
+        'descricao': descricao
+      });
+    }
+  };
 
 })
-   
-.controller('tirarFotoDoLancheCtrl', function($scope) {
+
+.controller('listagemCtrl', function($scope, $ionicListDelegate, Lanches) {
+  $scope.lanches = Lanches;
+  $scope.purchaseItem = function(lanche) {
+    var lancheRef = new Firebase('https://oquetemnacestadachap.firebaseio.com/lanches/' + lanche.$id);
+    lancheRef.child('status').set('purchased');
+    $ionicListDelegate.closeOptionButtons();
+  };
 
 })
-   
-.controller('listagemCtrl', function($scope) {
 
-})
-   
 .controller('detalheDoLancheColegaCtrl', function($scope) {
 
 })
-   
+
 .controller('detalheDoMeuLancheCtrl', function($scope) {
 
 })
- 
